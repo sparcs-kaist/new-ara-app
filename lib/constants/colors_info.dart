0756d3e055571ebf15c8a82e4bdc5d8b5d244966
skykhs3 <skykhs3@naver.com>
2023-05-03 03:03:48 +0900

    기능삭제: UrlInfo 삭제
    사용하지 않는 UrlInfo Class 삭제

--- lib/constants/colors_info.dart
+++ lib/constants/colors_info.dart
@@ -7,15 +7,3 @@ class ColorsInfo {
   static const Color gradBegin = Color.fromRGBO(237, 58, 58, 0.1);
   static const Color gradEnd = Color.fromRGBO(237, 58, 58, 0);
 }
-
-class UrlInfo {
-  static const BASE_URL = 'https://newara.dev.sparcs.org/api';
-  static const MAIN_URL = 'https://newara.dev.sparcs.org';
-
-  static const WEBVIEW_INIT =
-      "https://newara.dev.sparcs.org/api/users/sso_login/?next=https://newara.dev.sparcs.org/login-handler";
-
-  static const MAIN_AUTHORITY = 'newara.dev.sparcs.org';
-  static const AUTH_AUTHORITY = 'sparcssso.kaist.ac.kr';
-  static const KAISTIAM_AUTHORITY = 'iam2.kaist.ac.kr';
-}
