abcb02d0d69cb425cabdba9a4f94ce3c3b5c22ca
skykhs3 <skykhs3@naver.com>
2023-05-18 19:29:22 +0900

    Feat: Icons changes

--- lib/models/article_list_action_model.dart
+++ lib/models/article_list_action_model.dart
@@ -1,52 +1,50 @@
-import 'package:new_ara_app/models/board_model.dart';
-import 'package:new_ara_app/models/public_user.dart';
-import 'package:new_ara_app/models/topic_model.dart';
+class ArticleInfo {
+  final int id; // 글 마다 부여되는 id, 모든 글에 있는 듯함
+  final bool? is_hidden; // 숨김처리 되었는지 여부, 담아둔 글에서는 존재하지 않는다
+  final dynamic why_hidden; // 숨김처리 되었는지 여부, 담아둔 글에서는 존재하지 않는다
+  final dynamic can_override_hidden; // 변수의 타입 및 용도 정확히 모르겠음
+  final dynamic parent_topic; // 변수의 타입 정확하게 모르겠음
+  final Map<String, dynamic>? parent_board;
+  final String? title;
+  final Map<String, dynamic>? created_by;
+  final String? read_status;
+  final String? attachment_type;
+  final dynamic communication_article_status; // 변수의 타입 및 용도 정확하게 모르겠음
+  final dynamic days_left; // 변수의 타입 및 용도 정확하게 모르겠음
+  final String? created_at;
+  final String? updated_at;
+  final String? deleted_at;
+  final int? name_type;
+  final bool? is_content_sexual;
+  final bool? is_content_social;
+  final int? hit_count;
+  final int? comment_count;
+  final int? report_count;
+  final int? positive_vote_count;
+  final int? negative_vote_count;
+  final String? commented_at;
+  final dynamic url; // 변수의 타입 정확하게 모르겠음
+  final dynamic content_updated_at; // 변수의 타입 정확하게 모르겠음
+  final dynamic hidden; // 변수의 타입 정확하게 모르겠음
+  final Map<String, dynamic>? scrapped_by;
+  final dynamic parent_article;
 
-class ArticleListActionModel {
-  int? id;
-  bool? is_hidden;
-  String? why_hidden;
-  bool? can_override_hidden;
-  TopicModel? parent_topic;
-  BoardModel? parent_board;
-  String? title;
-  PublicUserModel? createdBy;
-  String? read_status;
-  String? attachment_type;
-  String? communication_article_status;
-  String? days_left;
-  String? created_at;
-  String? updated_at;
-  String? deleted_at;
-  int? name_type;
-  bool? is_content_sexual;
-  bool? is_content_social;
-  int? hit_count;
-  int? comment_count;
-  int? report_count;
-  int? positive_vote_count;
-  int? negative_vote_count;
-  String? commented_at;
-  String? url;
-  String? content_updated_at;
-  String? hidden_at;
-
-  ArticleListActionModel({
-    this.id,
+  ArticleInfo({
+    required this.id,
     this.is_hidden,
     this.why_hidden,
     this.can_override_hidden,
     this.parent_topic,
     this.parent_board,
     this.title,
-    this.createdBy,
+    this.created_by,
     this.read_status,
     this.attachment_type,
     this.communication_article_status,
     this.days_left,
-    this.created_at,
-    this.updated_at,
-    this.deleted_at,
+    required this.created_at,
+    required this.updated_at,
+    required this.deleted_at,
     this.name_type,
     this.is_content_sexual,
     this.is_content_social,
@@ -58,70 +56,70 @@ class ArticleListActionModel {
     this.commented_at,
     this.url,
     this.content_updated_at,
-    this.hidden_at,
+    this.hidden,
+    this.scrapped_by,
+    this.parent_article,
   });
 
-  factory ArticleListActionModel.fromJson(Map<String, dynamic> json) {
-    return ArticleListActionModel(
-      id: json['id'],
-      is_hidden: json['is_hidden'],
-      why_hidden: json['why_hidden'],
-      can_override_hidden: json['can_override_hidden'],
-      parent_topic: TopicModel.fromJson(json['parent_topic']),
-      parent_board: BoardModel.fromJson(json['parent_board']),
-      title: json['title'],
-      createdBy: PublicUserModel.fromJson(json['created_by']),
-      read_status: json['read_status'],
-      attachment_type: json['attachment_type'],
-      communication_article_status: json['communication_article_status'],
-      days_left: json['days_left'],
-      created_at: json['created_at'],
-      updated_at: json['updated_at'],
-      deleted_at: json['deleted_at'],
-      name_type: json['name_type'],
-      is_content_sexual: json['is_content_sexual'],
-      is_content_social: json['is_content_social'],
-      hit_count: json['hit_count'],
-      comment_count: json['comment_count'],
-      report_count: json['report_count'],
-      positive_vote_count: json['positive_vote_count'],
-      negative_vote_count: json['negative_vote_count'],
-      commented_at: json['commented_at'],
-      url: json['url'],
-      content_updated_at: json['content_updated_at'],
-      hidden_at: json['hidden_at'],
-    );
-  }
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'is_hidden': is_hidden,
-      'why_hidden': why_hidden,
-      'can_override_hidden': can_override_hidden,
-      'parent_topic': parent_topic?.toJson(),
-      'parent_board': parent_board?.toJson(),
-      'title': title,
-      'created_by': createdBy?.toJson(),
-      'read_status': read_status,
-      'attachment_type': attachment_type,
-      'communication_article_status': communication_article_status,
-      'days_left': days_left,
-      'created_at': created_at,
-      'updated_at': updated_at,
-      'deleted_at': deleted_at,
-      'name_type': name_type,
-      'is_content_sexual': is_content_sexual,
-      'is_content_social': is_content_social,
-      'hit_count': hit_count,
-      'comment_count': comment_count,
-      'report_count': report_count,
-      'positive_vote_count': positive_vote_count,
-      'negative_vote_count': negative_vote_count,
-      'commented_at': commented_at,
-      'url': url,
-      'content_updated_at': content_updated_at,
-      'hidden_at': hidden_at,
-    };
-  }
-}
+  ArticleInfo.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        is_hidden = json['is_hidden'],
+        why_hidden = json['why_hidden'],
+        can_override_hidden = json['can_override_hidden'],
+        parent_topic = json['parent_topic'],
+        parent_board = json['parent_board'],
+        title = json['title'],
+        created_by = json['created_by'],
+        read_status = json['read_status'],
+        attachment_type = json['attachment_type'],
+        communication_article_status = json['communication_article_status'],
+        days_left = json['days_left'],
+        created_at = json['created_at'],
+        updated_at = json['updated_at'],
+        deleted_at = json['deleted_at'],
+        name_type = json['name_type'],
+        is_content_sexual = json['is_content_sexual'],
+        is_content_social = json['is_content_social'],
+        hit_count = json['hit_count'],
+        comment_count = json['comment_count'],
+        report_count = json['report_count'],
+        positive_vote_count = json['positive_vote_count'],
+        negative_vote_count = json['negative_vote_count'],
+        commented_at = json['commented_at'],
+        url = json['url'],
+        content_updated_at = json['content_updated_at'],
+        hidden = json['hidden'],
+        scrapped_by = json['scrapped_by'],
+        parent_article = json['parent_article'];
 
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'is_hidden': is_hidden,
+        'can_override_hidden': can_override_hidden,
+        'parent_topic': parent_topic,
+        'parent_board': parent_board,
+        'title': title,
+        'created_by': created_by,
+        'read_status': read_status,
+        'attachment_type': attachment_type,
+        'communication_article_status': communication_article_status,
+        'days_left': days_left,
+        'created_at': created_at,
+        'updated_at': updated_at,
+        'deleted_at': deleted_at,
+        'name_type': name_type,
+        'is_content_sexual': is_content_sexual,
+        'is_content_social': is_content_social,
+        'hit_count': hit_count,
+        'comment_count': comment_count,
+        'report_count': report_count,
+        'positive_vote_count': positive_vote_count,
+        'negative_vote_count': negative_vote_count,
+        'commented_at': commented_at,
+        'url': url,
+        'content_updated_at': content_updated_at,
+        'hidden': hidden,
+        'scrapped_by': scrapped_by,
+        'parent_article': parent_article,
+      };
+}
