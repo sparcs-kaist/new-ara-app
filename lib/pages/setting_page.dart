9dab9cc18cac5cd59ae9e6834260d49f63b45d9b
Sang-oh Kim <tkddh1109@gmail.com>
2023-05-26 16:00:15 +0900

    Update setting_page.art: var -> UserProvider
    var 대신 UserProvider로 타입을 명시해두었음

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -21,23 +21,22 @@ class SettingPage extends StatefulWidget {
 }
 
 class SettingPageState extends State<SettingPage> {
-
-  bool see_sexual=true;
-  bool see_social=true;
+  bool see_sexual = true;
+  bool see_social = true;
 
   @override
   void initState() {
-    var userProvider = context.read<UserProvider>();
+    UserProvider userProvider = context.read<UserProvider>();
     // TODO: implement initState
     super.initState();
-    see_sexual = userProvider.naUser!.see_sexual; // 웹과 동일하게 하기 위해 snake_case 변수명 사용
-    see_social  = userProvider.naUser!.see_social; // 위와 같은 이유로 snake_case
-
+    see_sexual =
+        userProvider.naUser!.see_sexual; // 웹과 동일하게 하기 위해 snake_case 변수명 사용
+    see_social = userProvider.naUser!.see_social; // 위와 같은 이유로 snake_case
   }
 
   @override
   Widget build(BuildContext context) {
-    var userProvider = context.watch<UserProvider>();
+    UserProvider userProvider = context.watch<UserProvider>();
 
     var dio = Dio();
     dio.options.headers['Cookie'] = userProvider.getCookiesToString();
@@ -176,7 +175,9 @@ class SettingPageState extends State<SettingPage> {
                                   activeColor: ColorsInfo.newara,
                                   value: see_social,
                                   onChanged: (value) async {
-                                    setState((){see_social = value;});
+                                    setState(() {
+                                      see_social = value;
+                                    });
                                     try {
                                       await dio.patch(
                                           '$newAraDefaultUrl/api/user_profiles/${userProvider.naUser!.user}/',
