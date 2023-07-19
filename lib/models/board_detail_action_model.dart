c37ca2d2962714d31e014adbff6c85d2d330145a
Sang-oh Kim <tkddh1109@gmail.com>
2023-07-19 20:44:58 +0900

    Fix(board_detail_action_model.dart): 백엔드 업데이트 반영
    백엔드에서 모델 업데이트가 있어서 앱에서도 반영함.

--- lib/models/board_detail_action_model.dart
+++ lib/models/board_detail_action_model.dart
@@ -2,56 +2,34 @@ import 'package:new_ara_app/models/topic_model.dart';
 
 class BoardDetailActionModel {
   final int id;
-  final List<TopicModel> topics;
-  final bool user_readable;
-  final bool user_writable;
-  final String created_at; // $date-time
-  final String updated_at; // $date-time
-  final String deleted_at; // $date-time, default
   final String slug;
   final String? ko_name;
   final String? en_name;
-  final String ko_description;
-  final String en_description;
-  final int? read_access_mask;
-  final int? write_access_mask;
-  final int? comment_access_mask;
   final bool? is_readonly; // 활성화했을 때 관리자만 글을 쓸 수 있음
-  final bool? is_hidden; // 활성화했을 때 메인페이지 상단바 리스트에 나타나지 않음
   final int? name_type;
-  final bool? is_school_communication;
   final int? group_id;
   final String? banner_image; // $uri
   final String? ko_banner_description;
   final String? en_banner_description;
-  final String? banner_url;
   final int? top_threshold; // 인기글 달성 기준 좋아요 갯수
+  final List<TopicModel> topics;
+  final bool user_readable;
+  final bool user_writable;
 
   BoardDetailActionModel({
     required this.id,
     required this.topics,
     required this.user_readable,
     required this.user_writable,
-    required this.created_at,
-    required this.updated_at,
-    this.deleted_at = "0001-01-01T08:27:52+08:27:52",
     required this.slug,
     required this.ko_name,
     required this.en_name,
-    required this.ko_description,
-    required this.en_description,
-    this.read_access_mask,
-    this.write_access_mask,
-    this.comment_access_mask,
     this.is_readonly,
-    this.is_hidden,
     this.name_type,
-    this.is_school_communication,
     this.group_id,
     this.banner_image,
     this.ko_banner_description,
     this.en_banner_description,
-    this.banner_url,
     this.top_threshold,
   });
 
@@ -63,26 +41,15 @@ class BoardDetailActionModel {
           .toList(),
       user_readable: json['user_readable'],
       user_writable: json['user_writable'],
-      created_at: json['created_at'],
-      updated_at: json['updated_at'],
-      deleted_at: json['deleted_at'],
       slug: json['slug'],
       ko_name: json['ko_name'],
       en_name: json['en_name'],
-      ko_description: json['ko_description'],
-      en_description: json['en_description'],
-      read_access_mask: json['read_access_mask'],
-      write_access_mask: json['write_access_mask'],
-      comment_access_mask: json['comment_access_mask'],
       is_readonly: json['is_readonly'],
-      is_hidden: json['is_hidden'],
       name_type: json['name_type'],
-      is_school_communication: json['is_school_communication'],
       group_id: json['group_id'],
       banner_image: json['banner_image'],
       ko_banner_description: json['ko_banner_description'],
       en_banner_description: json['en_banner_description'],
-      banner_url: json['banner_url'],
       top_threshold: json['top_threshold'],
     );
   }
@@ -93,26 +60,15 @@ class BoardDetailActionModel {
       'topics': topics.map((topic) => topic.toJson()).toList(),
       'user_readable': user_readable,
       'user_writable': user_writable,
-      'created_at': created_at,
-      'updated_at': updated_at,
-      'deleted_at': deleted_at,
       'slug': slug,
       'ko_name': ko_name,
       'en_name': en_name,
-      'ko_description': ko_description,
-      'en_description': en_description,
-      'read_access_mask': read_access_mask,
-      'write_access_mask': write_access_mask,
-      'comment_access_mask': comment_access_mask,
       'is_readonly': is_readonly,
-      'is_hidden': is_hidden,
       'name_type': name_type,
-      'is_school_communication': is_school_communication,
       'group_id': group_id,
       'banner_image': banner_image,
       'ko_banner_description': ko_banner_description,
       'en_banner_description': en_banner_description,
-      'banner_url': banner_url,
       'top_threshold': top_threshold,
     };
   }
