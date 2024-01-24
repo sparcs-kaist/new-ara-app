3c62808b74ea586aded8763ed459e54b140a1678
Sang-oh Kim <tkddh1109@gmail.com>
2024-01-24 15:18:31 +0900

    Feat(sparcs_sso_page.dart): 소셜 로그인 비활성화 구현
    기존에 만들어놓았던 getclientIDState 함수는 필요 없어져서 제거함. WebView의 onPageFinished, onPageStarted 메서드에 관련 조건을 추가하여 소셜 로그인 비활성화를 구현함.

--- lib/pages/sparcs_sso_page.dart
+++ lib/pages/sparcs_sso_page.dart
@@ -29,80 +29,90 @@ class _SparcsSSOPageState extends State<SparcsSSOPage> {
   // TODO: 이 userID가 왜 쓰이는 지, 왜 남겨두었는지 알아보기
   int userID = 0;
 
-  Future<String> getClientIDState() async {
-    // /api/sso_login으로 요청 보내기
-    Dio _dio = Dio();
-    _dio.options.followRedirects = true;
-    _dio.options.responseType = ResponseType.plain;
-    final String apiUrl = "https://newara.dev.sparcs.org/api/users/sso_login/";
-    var response = await _dio.get(apiUrl);
-    return response.realUri.toString();
-  }
-
   @override
   void initState() {
     super.initState();
 
-    getClientIDState().then((path) {
-      debugPrint('real path: $path');
-      
-      // 쿠키 초기화
-      // 다른 쿠키도 모두 초기화되는지 알아야 함
-      WebViewCookieManager().clearCookies();
+    debugPrint("initState invoked!!!");
 
-      _controller = WebViewController()
-        // JavaScript 활성화
-        ..setJavaScriptMode(JavaScriptMode.unrestricted)
-        // WebView 배경색 설정
-        ..setBackgroundColor(const Color(0x00000000))
-        // 네비게이션 대리자 설정
-        ..setNavigationDelegate(NavigationDelegate(
-          // 페이지 로딩 상태를 출력
-          onProgress: (int progress) {
-            debugPrint('WebView가 로딩 중입니다 (진행률: $progress)');
-          },
-          // 페이지 로딩 시작 시
-          onPageStarted: (String url) {
-            debugPrint("Start to load $url");
-            // 로딩 표시기 활성화
-            setState(() => isVisible = false);
-          },
-          // 페이지 로딩 완료 시
-          onPageFinished: (String url) async {
-            // 특정 URL로 끝나면 로그인 성공으로 판단
-            if (url.endsWith('$newAraDefaultUrl/') && mounted) {
-              var userProvider = context.read<UserProvider>();
-              debugPrint("main.dart:로그인 성공");
+    // 쿠키 초기화
+    // 다른 쿠키도 모두 초기화되는지 알아야 함
+    WebViewCookieManager().clearCookies();
 
-              FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+    _controller = WebViewController()
+      // JavaScript 활성화
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      // WebView 배경색 설정
+      ..setBackgroundColor(const Color(0x00000000))
+      // 네비게이션 대리자 설정
+      ..setNavigationDelegate(NavigationDelegate(
+        // 페이지 로딩 상태를 출력
+        onProgress: (int progress) {
+          debugPrint('WebView가 로딩 중입니다 (진행률: $progress)');
+        },
+        // 페이지 로딩 시작 시
+        onPageStarted: (String url) async {
+          debugPrint("Start to load $url");
+          // 로딩 표시기 활성화
+          setState(() => isVisible = false);
+          /* 
+          로그인 페이지에서 SPARCS SSO 내의 다른 페이지를 방문하였다가 다시 로그인 페이지로 돌아오는 경우,
+          client_id, state가 아직 발급되지 않은 상태이기 때문에 소셜 로그인 비활성화를 할 수 없음.
+          따라서 /api/users/sso_login/으로 리다이렉트함.
+           */
+          if (url == 'https://sparcssso.kaist.ac.kr/account/login/') {
+            await _controller.runJavaScript(
+                "window.location.href = '$newAraDefaultUrl/api/users/sso_login/'");
+          }
+        },
+        // 페이지 로딩 완료 시
+        onPageFinished: (String url) async {
+          debugPrint("Finish loading url: $url");
+          // 특정 URL로 끝나면 로그인 성공으로 판단
+          if (url.endsWith('$newAraDefaultUrl/') && mounted) {
+            var userProvider = context.read<UserProvider>();
+            debugPrint("main.dart:로그인 성공");
 
-              debugPrint("현재 URL은 $url 입니다");
+            FlutterSecureStorage secureStorage = const FlutterSecureStorage();
 
-              await userProvider.setCookiesFromUrl(url);
-              await userProvider.apiMeUserInfo(message: "sparscssso");
-              userProvider.setHasData(true);
+            debugPrint("현재 URL은 $url 입니다");
 
-              String cookieString = userProvider.getCookiesToString();
-              await secureStorage.write(key: 'cookie', value: cookieString);
-            } else if (mounted) {
-              debugPrint("Finish loading url: $url");
-              // 로딩 완료 후 WebView 활성화
-              setState(() {
-                isVisible = true;
-              });
-            } else {
-              debugPrint("not mounted");
+            await userProvider.setCookiesFromUrl(url);
+            await userProvider.apiMeUserInfo(message: "sparscssso");
+            userProvider.setHasData(true);
+
+            String cookieString = userProvider.getCookiesToString();
+            await secureStorage.write(key: 'cookie', value: cookieString);
+          } else if (mounted) {
+            String curUrl = (await _controller.currentUrl()).toString();
+            debugPrint('curUrl: $curUrl');
+            /* /api/users/sso_login/가 성공적으로 실행되어 client_id, state를 발급받았지만
+               소셜 로그인 비활성화가 되어있지 않은 경우, 관련 파리미터를 추가하려 리로드함. */
+            if (curUrl.startsWith(
+                    'https://sparcssso.kaist.ac.kr/account/login/?next=/api/v2/token/require/') &&
+                curUrl.contains('social_enabled') == false) {
+              debugPrint("NO SOCIAL ENABLED");
+              // 소설 로그인 비활성화를 위해 social_enabled, show_disabled_button 파라미터를 모두 0으로 설정.
+              await _controller.runJavaScript(
+                  "window.location.href = '$curUrl%26social_enabled%3D0%26show_disabled_button%3D0';");
             }
-          },
-          // WebView 리소스 오류 발생 시
-          onWebResourceError: (WebResourceError error) {
-            debugPrint(
-                '코드: ${error.errorCode}\n설명: ${error.description}\n오류 유형: ${error.errorType}\n메인 프레임에 대한 것인가? ${error.isForMainFrame}');
-          },
-        ))
-        // 시작 URL 로딩
-        ..loadRequest(Uri.parse('https://sparcssso.kaist.ac.kr$path%26social_enabled%3D0%26show_disabled_button%3D0'));
-    });
+            // 로딩 완료 후 WebView 활성화
+            setState(() {
+              isVisible = true;
+            });
+          } else {
+            debugPrint("not mounted");
+          }
+        },
+        // WebView 리소스 오류 발생 시
+        onWebResourceError: (WebResourceError error) {
+          debugPrint(
+              '코드: ${error.errorCode}\n설명: ${error.description}\n오류 유형: ${error.errorType}\n메인 프레임에 대한 것인가? ${error.isForMainFrame}');
+        },
+      ))
+      // 시작 URL 로딩
+      ..loadRequest(
+          Uri.parse('https://newara.dev.sparcs.org/api/users/sso_login/'));
   }
 
   @override
