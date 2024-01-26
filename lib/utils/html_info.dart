1146edf5dce33c6c07612c61aa09bade501efae0
Sang-oh Kim <tkddh1109@gmail.com>
2024-01-26 16:20:13 +0900

    Comment(html_info.dart): u태그 처리 로직 주석 추가

--- lib/utils/html_info.dart
+++ lib/utils/html_info.dart
@@ -2,12 +2,19 @@ import 'package:sanitize_html/sanitize_html.dart' show sanitizeHtml;
 import 'package:flutter/material.dart';
 
 String getContentHtml(String content, {double? width}) {
+  /* sanitize_html 패키지에서는 <u> 태그를 제거해버림
+     (sanitize_html은 다소 strict하게 sanitizing을 시행함).
+     따라서 <u>의 sanitizing을 피하기 위해 sanitize_html이 허용하는 태그
+     (sanitize_html 패키지 구현 코드에서 _allowedElements 리스트 확인하기) 중 사용되지 않을 확률이 높은
+     <h8> 태그를 사용함(HTML 표준이 아니라 사용될 확률이 거의 없음).
+     Sanitizing 직전에 모든 u 태그를 h8 태그로 변경 후 sanitize 후에 다시 추가하는 방식.
+     <u> 태그 자체에 XSS 공격 취약점이 있는 것이 아니고 아래 과정에서도 모든 attribute는 지워지므로 어느정도 안전한 방식. */
   String replacedContent = content
-      .replaceAll(RegExp('<u(?: [^>]*)?>'), '<sup>')
-      .replaceAll('</u>', '</sup>');
+      .replaceAll(RegExp('<u(?: [^>]*)?>'), '<h8>')
+      .replaceAll('</u>', '</h8>');
   String sanitized = sanitizeHtml(replacedContent)
-      .replaceAll('<sup>', '<u>')
-      .replaceAll('</sup>', '</u>');
+      .replaceAll('<h8>', '<u>')
+      .replaceAll('</h8>', '</u>');
 
   debugPrint("original html: $content");
   debugPrint("sanitized html: $sanitized");
