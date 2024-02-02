094d3f31be1d900e0b1eacb82b1bb1e0959c6e10
Sang-oh Kim <tkddh1109@gmail.com>
2024-02-02 22:12:23 +0900

    Refactor(handle_hidden.dart): 상수값 const 추가
    상수인 hiddenReasons, hiddenReasonsComment, hiddenReasonNotices에 const 키워드를 추가함.

--- lib/utils/handle_hidden.dart
+++ lib/utils/handle_hidden.dart
@@ -1,6 +1,6 @@
 /// (2023.02.01) 현재 BE에 있는 모든 글 숨김 사유
 /// BE가 변경되면 새로 확인해볼 필요 있음.
-Map<String, String> hiddenReasons = {
+const Map<String, String> hiddenReasons = {
   "REPORTED_CONTENT": "신고 누적으로 숨김된 게시물입니다.",
   "BLOCKED_USER_CONTENT": "차단한 사용자의 게시물입니다.",
   "ADULT_CONTENT": "성인/음란성 내용의 게시물입니다.",
@@ -10,14 +10,14 @@ Map<String, String> hiddenReasons = {
 
 /// (2023.02.01) 현재 BE에 있는 모든 댓글 숨김 사유
 /// BE가 변경되면 새로 확인해볼 필요 있음.
-Map<String, String> hiddenReasonsComment = {
+const Map<String, String> hiddenReasonsComment = {
   "REPORTED_CONTENT": "신고 누적으로 숨김된 댓글입니다.",
   "BLOCKED_USER_CONTENT": "차단한 사용자의 댓글입니다.",
   "DELETED_CONTENT": "삭제된 댓글입니다.",
 };
 
 /// (2023.02.01) 현재 BE에 있는 글 숨김 사유에 따른 사용자 안내 메시지
-Map<String, String> hiddenReasonNotices = {
+const Map<String, String> hiddenReasonNotices = {
   "REPORTED_CONTENT": "",
   "BLOCKED_USER_CONTENT": "차단 사용자 설정은 설정페이지에서 하실 수 있습니다.",
   "ADULT_CONTENT": "게시글 보기 설정은 설정페이지에서 하실 수 있습니다.",
