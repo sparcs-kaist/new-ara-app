1bd8f8f49af1b6a75ca06c58c23d77f2512e8630
Sang-oh Kim <tkddh1109@gmail.com>
2024-02-03 12:31:27 +0900

    Design, Refactor(snackbar_noti.dart): 확장성 개선, 디자인 수정
    피그마 디자인을 더 반영하며 확장성 있도록 함.

--- lib/widgets/snackbar_noti.dart
+++ lib/widgets/snackbar_noti.dart
@@ -1,26 +1,16 @@
 import 'package:flutter/material.dart';
 
-void showAraSnackBar(BuildContext context, String displayContent) {
+void showAraSnackBar(BuildContext context, Widget displayContent) {
   final araSnackBar = SnackBar(
-    content: Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(16)),
-        border: Border.all(
-          color: const Color(0xFFF0F0F0),
-          width: 0.5,
-        ),
-      ),
-      width: 358,
-      height: 30,
-      child: Text(
-        displayContent,
-        style: const TextStyle(
-          color: Colors.black,
-        ),
-      ),
+    content: Center(
+      child: SizedBox(
+          width: MediaQuery.of(context).size.width - 34, child: displayContent),
     ),
+    margin: const EdgeInsets.only(left: 16, right: 16, bottom: 20),
     behavior: SnackBarBehavior.floating,
     backgroundColor: Colors.white,
+    shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(16))),
   );
   ScaffoldMessenger.of(context).showSnackBar(araSnackBar);
 }
