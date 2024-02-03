a95c189cab242e8e95e14e2ba38e1d1fa4c2cd19
Sang-oh Kim <tkddh1109@gmail.com>
2024-02-03 12:45:24 +0900

    Design(snackbar_noti.dart): padding 추가
    피그마 디자인과 유사하도록 패딩 설정

--- lib/widgets/snackbar_noti.dart
+++ lib/widgets/snackbar_noti.dart
@@ -7,10 +7,13 @@ void showAraSnackBar(BuildContext context, Widget displayContent) {
           width: MediaQuery.of(context).size.width - 34, child: displayContent),
     ),
     margin: const EdgeInsets.only(left: 16, right: 16, bottom: 20),
+    padding: const EdgeInsets.only(top: 15, bottom: 15, left: 12),
     behavior: SnackBarBehavior.floating,
     backgroundColor: Colors.white,
     shape: const RoundedRectangleBorder(
+      side: BorderSide(color: Color(0xFFF0F0F0), width: 0.5),
         borderRadius: BorderRadius.all(Radius.circular(16))),
   );
+
   ScaffoldMessenger.of(context).showSnackBar(araSnackBar);
 }
