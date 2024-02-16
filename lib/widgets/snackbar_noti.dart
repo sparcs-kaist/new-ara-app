5587fab41f708a565a3680bdd47a66329204e834
Sang-oh Kim <tkddh1109@gmail.com>
2024-02-17 00:56:13 +0900

    Fix(snackbar_noti.dart): hideOldsAndShowAraSnackBar 리턴값 제거

--- lib/widgets/snackbar_noti.dart
+++ lib/widgets/snackbar_noti.dart
@@ -82,9 +82,9 @@ SnackBar buildAraSnackBar(context,
 
 /// 기존에 존재하는 스낵바로 인해 새로 호출한 스낵바가 queued되었다가 나중에 표시되는 현상을 방지하기 위해 사용.
 /// 새로운 스낵바 표시 전에 기존의 스낵바를 모두 숨김처리함.
-ScaffoldFeatureController<SnackBar, SnackBarClosedReason> hideOldsAndShowAraSnackBar(context, SnackBar araSnackBar) {
+void hideOldsAndShowAraSnackBar(context, SnackBar araSnackBar) {
   ScaffoldMessenger.of(context).hideCurrentSnackBar();
-  return ScaffoldMessenger.of(context).showSnackBar(araSnackBar);
+  ScaffoldMessenger.of(context).showSnackBar(araSnackBar);
 }
 
 /// information.svg와 함께 동일한 위젯 구성을 가진 SnackBar가 자주 사용되어
