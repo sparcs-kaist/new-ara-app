63f3d2d1ffff64015e36619c14456f11b070a79c
Sang-oh Kim <tkddh1109@gmail.com>
2024-02-16 17:10:36 +0900

    Fix(post_view_page.dart): 중복된 스낵바 삭제
    launchInBrowser가 실패했을 때 스낵바가 두 번 사용되어 하나를 제거함.

--- lib/pages/post_view_page.dart
+++ lib/pages/post_view_page.dart
@@ -1758,9 +1758,7 @@ class _PostViewPageState extends State<PostViewPage> {
       Uri.parse(url),
       mode: LaunchMode.externalApplication,
     )) {
-      // TODO: debugPrint 및 Toast message로 수정하기
       debugPrint('Could not launch $url');
-      _showInfoBySnackBar('브라우저로 URL을 열 수 없습니다.');
       return false;
     }
 
