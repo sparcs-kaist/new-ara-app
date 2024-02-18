f6d1b96121968bf0a82404733e90b170d69a182f
skykhs3 <skykhs3@naver.com>
2024-02-18 22:20:44 +0900

    Feat: SparcsSSOPage에서 LoginPage로 갈 수 있도록(전 페이지로 갈 수 있도록) 기능 추가

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,6 +4,7 @@ import 'package:easy_localization/easy_localization.dart';
 
 import 'package:new_ara_app/constants/colors_info.dart';
 import 'package:new_ara_app/pages/sparcs_sso_page.dart';
+import 'package:new_ara_app/utils/slide_routing.dart';
 
 /// `LoginPage` 위젯은 사용자에게 로그인 페이지를 표시.
 class LoginPage extends StatefulWidget {
@@ -16,56 +17,53 @@ class LoginPage extends StatefulWidget {
 
 /// `_LoginPageState` 클래스는 `LoginPage` 위젯의 상태를 관리.
 class _LoginPageState extends State<LoginPage> {
-  /// SSO 페이지를 표시할지 여부를 결정하는 변수.
-  bool showSSOPage = false;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: showSSOPage == false
-            ? Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: SvgPicture.asset(
-                        fit: BoxFit.contain,
-                        width: 200,
-                        'assets/images/logo.svg',
-                      ),
-                    ),
-                    Container(
-                      width: 300,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        color: ColorsInfo.newara,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: TextButton(
-                        child: Text(
-                          'login_page.login'.tr(),
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            showSSOPage = true;
-                          });
-                        },
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                  ],
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: SvgPicture.asset(
+                  fit: BoxFit.contain,
+                  width: 200,
+                  'assets/images/logo.svg',
                 ),
-              )
-            : const SparcsSSOPage(),
+              ),
+              Container(
+                width: 300,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: ColorsInfo.newara,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: TextButton(
+                  child: Text(
+                    'login_page.login'.tr(),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      slideRoute(
+                        const SparcsSSOPage(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
