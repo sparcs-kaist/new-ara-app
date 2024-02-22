86a89088a102537a9da3ac8a7112fb64543457d4
skykhs3 <skykhs3@naver.com>
2024-02-22 09:48:42 +0900

    Feat: 로그인 시 탈퇴 계정이면 문의 페이지로 이동
    로직
    1. sso 로그인
    2. api/me로 확인
    3. 유저 모델의 deleted_at 이 1900년 보다 크면 유저 앱 진입 막고 문의하기 페이지로 연결

---
+++ lib/pages/inquiry_page.dart
@@ -0,0 +1,105 @@
+import 'dart:io';
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:new_ara_app/constants/colors_info.dart';
+import 'package:new_ara_app/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class InQuiryPage extends StatefulWidget {
+  const InQuiryPage({super.key});
+
+  @override
+  State<InQuiryPage> createState() => _InQuiryPageState();
+}
+
+class _InQuiryPageState extends State<InQuiryPage> {
+  @override
+  Widget build(BuildContext context) {
+    UserProvider userProvider = context.watch<UserProvider>();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const SizedBox(
+          child: Text(
+            "문의 페이지",
+            style: TextStyle(
+              color: ColorsInfo.newara,
+              fontSize: 18,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        leadingWidth: 100,
+        leading: GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: () => Navigator.pop(context),
+          child: Stack(
+            alignment: Alignment.centerLeft,
+            children: [
+              SvgPicture.asset(
+                'assets/icons/left_chevron.svg',
+                colorFilter: const ColorFilter.mode(
+                    ColorsInfo.newara, BlendMode.srcATop),
+                fit: BoxFit.fill,
+                width: 35,
+                height: 35,
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text.rich(
+                TextSpan(
+                  text: '메일로 문의하세요!\n여기를 클릭하세요.',
+                  style: const TextStyle(
+                    decoration: TextDecoration.underline,
+                    color: Colors.blue, fontSize: 20),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () async {
+                      int? userID = userProvider.naUser!.user;
+                      String? email = userProvider.naUser?.email;
+                      String? nickname = userProvider.naUser?.nickname;
+
+                      final String body =
+                          """여기 아래에 문의 사항을 적어주세요.\n\n※ Ara 관리자가 48시간 이내로 답변드립니다.※\n\n유저 번호: $userID\n닉네임: $nickname\n이메일: $email\n플랫폼: App\n""";
+                      final emailLaunchUri = Uri(
+                        scheme: 'mailto',
+                        path: 'ara@sparcs.org',
+                        query: encodeQueryParameters(<String, String>{
+                          'subject': 'Ara에 문의합니다',
+                          'body': body,
+                        }),
+                      );
+
+                      if (!await launchUrl(
+                        emailLaunchUri,
+                      )) {
+                        debugPrint('Could not launch mail');
+                        debugPrint("기본 메일앱을 열 수 없습니다.");
+                      }
+                    },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+String? encodeQueryParameters(Map<String, String> params) {
+  return params.entries
+      .map((MapEntry<String, String> e) =>
+          '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+      .join('&');
+}
--- lib/pages/sparcs_sso_page.dart
+++ lib/pages/sparcs_sso_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:new_ara_app/constants/url_info.dart';
 import 'package:new_ara_app/pages/terms_and_conditions_page.dart';
+import 'package:new_ara_app/pages/inquiry_page.dart';
 import 'package:new_ara_app/providers/user_provider.dart';
 import 'package:new_ara_app/utils/slide_routing.dart';
 import 'package:webview_cookie_manager/webview_cookie_manager.dart';
@@ -96,7 +97,20 @@ class _SparcsSSOPageState extends State<SparcsSSOPage> {
             if (mounted) {
               // 이용약관 동의 시 현재 sso 로그인 창을 닫음
               if (url.endsWith('$newAraDefaultUrl/') == true) {
-                Navigator.of(context).pop();
+                DateTime dateTime =
+                    DateTime.parse(userProvider.naUser!.deleted_at);
+                // 1900년 1월 1일을 나타내는 DateTime 객체 생성
+                DateTime year1900 = DateTime(1900);
+
+                // 회원 탈퇴일이 1900년 1월 1일 이전이라면 탈퇴를 하지 않은 유저
+                if (dateTime.isBefore(year1900)) {
+
+                  Navigator.of(context).pushReplacement(slideRoute(const InQuiryPage()));
+                } else {
+                  // 회원 탈퇴일이 1900년 1월 1일 이후라면 탈퇴한 유저
+                  Navigator.of(context).pop();
+                  return;
+                }
               } else {
                 //이용약관 전이라면 현재 sso 로그인 창을 닫고 이용약관 페이지로 이동
                 Navigator.of(context).pushReplacement(
