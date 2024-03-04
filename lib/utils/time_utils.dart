b6e421a59cc60194ce815e29bdc6a72d10223ed2
skykhs3 <skykhs3@naver.com>
2024-03-05 00:12:20 +0900

    Fix: time error
    기존 : 현재 날짜(2024.03.05)이고 게시물 날짜(2024.02.05)일 경우 HH:MM으로 표현 됨(날짜가 표현 되지 않음)
    수정 : 버그 수정

--- lib/utils/time_utils.dart
+++ lib/utils/time_utils.dart
@@ -4,7 +4,8 @@ import 'package:easy_localization/easy_localization.dart';
 /// {}년 {}월 {}일 {}:{} 형식으로 변환하여 리턴.
 String specificTime(String rawTime) {
   DateTime date = DateTime.parse(rawTime).toLocal();
-  String time = '${DateFormat('yyyy').format(date)}년 ${DateFormat('MM').format(date)}월 ${DateFormat('dd').format(date)}일 ${DateFormat('HH').format(date)}:${DateFormat('mm').format(date)}';
+  String time =
+      '${DateFormat('yyyy').format(date)}년 ${DateFormat('MM').format(date)}월 ${DateFormat('dd').format(date)}일 ${DateFormat('HH').format(date)}:${DateFormat('mm').format(date)}';
   return time;
 }
 
@@ -18,7 +19,9 @@ String getTime(String rawTime) {
     time = "${difference.inSeconds}초 전";
   } else if (difference.inHours < 1) {
     time = '${difference.inMinutes}분 전';
-  } else if (date.day == now.day) {
+  } else if (date.year == now.year &&
+      date.month == now.month &&
+      date.day == now.day) {
     time = '${DateFormat('HH').format(date)}:${DateFormat('mm').format(date)}';
   } else if (date.year == now.year) {
     time = '${DateFormat('MM').format(date)}/${DateFormat('dd').format(date)}';
