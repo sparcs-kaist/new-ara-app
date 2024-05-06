c5dcdd834387f035959ea145ef552b1a02162fff
PeckaOW <92661606+thomaskim1130@users.noreply.github.com>
2024-05-06 23:45:33 +0900

    Fix(slide_routing.dart): Flickering 이슈 일부 해결
    Android에서, page간 slideRoute를 사용할 때의 깜빡임 현상을 background color를 고정해 일부 해결함

--- lib/utils/slide_routing.dart
+++ lib/utils/slide_routing.dart
@@ -1,24 +1,27 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 
 Route slideRoute(Widget dst) {
   // iOS, Android에서 모두 back swipe 적용
   // TODO: Android에서 back swipe 적용할지 여부 논의 (2023.01.24)
-  return CupertinoPageRoute(
-    builder: (context) => dst,
-  );
-  // return PageRouteBuilder(
-  //   pageBuilder: (context, animation, secondaryAnimation) => dst,
-  //   transitionsBuilder: ((context, animation, secondaryAnimation, child) {
-  //     var begin = const Offset(1, 0);
-  //     var end = const Offset(0, 0);
-  //     var curve = Curves.ease;
+  if (Platform.isIOS) {
+    return CupertinoPageRoute(
+      builder: (context) => dst,
+    );
+  }
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => dst,
+    transitionsBuilder: ((context, animation, secondaryAnimation, child) {
+      var begin = const Offset(1, 0);
+      var end = const Offset(0, 0);
+      var curve = Curves.ease;
 
-  //     var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-  //     var offsetAnimation = animation.drive(tween);
-  //     return SlideTransition(
-  //       position: offsetAnimation,
-  //       child: child,
-  //     );
-  //   }),
-  // );
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      var offsetAnimation = animation.drive(tween);
+      return SlideTransition(
+        position: offsetAnimation,
+        child: child,
+      );
+    }),
+  );
 }
