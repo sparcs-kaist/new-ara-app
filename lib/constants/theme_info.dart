94caea04d0522ed5e98e317a66d08e5f24b70c97
edge <edge@sparcs.org>
2024-11-14 23:24:07 +0900

    Feature: add text theme data

--- lib/constants/theme_info.dart
+++ lib/constants/theme_info.dart
@@ -21,6 +21,42 @@ class NewAraThemes {
       displaySmall: TextStyle(
         color: Colors.white,
       ),
+      headlineLarge: TextStyle(
+        color: Colors.white,
+      ),
+      headlineMedium: TextStyle(
+        color: Colors.white,
+      ),
+      headlineSmall: TextStyle(
+        color: Colors.white,
+      ),
+      titleLarge: TextStyle(
+        color: Colors.white,
+      ),
+      titleMedium: TextStyle(
+        color: Colors.white,
+      ),
+      titleSmall: TextStyle(
+        color: Colors.white,
+      ),
+      bodyLarge: TextStyle(
+        color: Colors.white,
+      ),
+      bodyMedium: TextStyle(
+        color: Colors.white,
+      ),
+      bodySmall: TextStyle(
+        color: Colors.white,
+      ),
+      labelLarge: TextStyle(
+        color: Colors.white,
+      ),
+      labelMedium: TextStyle(
+        color: Colors.white,
+      ),
+      labelSmall: TextStyle(
+        color: Colors.white,
+      ),
     ),
   );
 
@@ -34,7 +70,53 @@ class NewAraThemes {
     textSelectionTheme: const TextSelectionThemeData(
       cursorColor: Colors.transparent,
     ),
-
+    textTheme: TextTheme(
+      displayLarge: TextStyle(
+        color: Colors.black,
+      ),
+      displayMedium: TextStyle(
+        color: Colors.black,
+      ),
+      displaySmall: TextStyle(
+        color: Colors.black,
+      ),
+      headlineLarge: TextStyle(
+        color: Colors.black,
+      ),
+      headlineMedium: TextStyle(
+        color: Colors.black,
+      ),
+      headlineSmall: TextStyle(
+        color: Colors.black,
+      ),
+      titleLarge: TextStyle(
+        color: Colors.black,
+      ),
+      titleMedium: TextStyle(
+        color: Colors.black,
+      ),
+      titleSmall: TextStyle(
+        color: Colors.black,
+      ),
+      bodyLarge: TextStyle(
+        color: Colors.black,
+      ),
+      bodyMedium: TextStyle(
+        color: Colors.black,
+      ),
+      bodySmall: TextStyle(
+        color: Colors.black,
+      ),
+      labelLarge: TextStyle(
+        color: Colors.black,
+      ),
+      labelMedium: TextStyle(
+        color: Colors.black,
+      ),
+      labelSmall: TextStyle(
+        color: Colors.black,
+      ),
+    )
   );
 
 
