47722bc69bc2cdfdd8e82f58bb914446e47fae75
Sang-oh Kim <tkddh1109@gmail.com>
2024-11-14 23:13:29 +0900

    Chore: Upgrade AGP version to 8.1.0
    AGP version, Kotlin version 및 일부 패키지의 버전을 업그레이드함.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
