c2f9c76dda3afa0e4f66b7513421025b0333d430
edge <edge@sparcs.org>
2024-11-21 22:41:32 +0900

    Chore: remove comment

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 
-//---This to switch theme from Switch button----
 class ThemeProvider extends ChangeNotifier {
 
-  //-----Store the theme of our app--
   ThemeMode? themeMode = null;
 
-  //----If theme mode is equal to dark then we return True----
-  //-----isDarkMode--is the field we will use in our switch---
   bool _isDarkMode = false;
-  // bool get isDarkMode => themeMode == ThemeMode.dark;
+
   bool get isDarkMode => _isDarkMode;
 
   void setTheme(bool change) {
@@ -19,13 +15,11 @@ class ThemeProvider extends ChangeNotifier {
   }
 
 
-  //---implement ToggleTheme function----
   void updateTheme() {
 
     _isDarkMode = !_isDarkMode;
     themeMode = _isDarkMode ? ThemeMode.dark : ThemeMode.light;
-
-    //---notify material app to update UI----
+g
     notifyListeners();
   }
 
